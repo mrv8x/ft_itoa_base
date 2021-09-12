8c95957b5f49240b109420b6fd3817cfe525da90
mrv8x <57045314+mrv8xx@users.noreply.github.com>
2021-09-12 01:10:09 +0100

    First commit

---
+++ ft_itoa_base.c
@@ -0,0 +1,78 @@
+
+#include <stdlib.h>
+
+int		ft_nbr_len(int nb, int base)
+{
+	int i;
+
+	i = 0;
+	if (nb < 0)
+		nb *= -1;
+	while (nb > 0)
+	{
+		nb /= base;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *str)
+{
+	int		i;
+	int		len;
+	char	*cpy;
+
+	i = 0;
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	cpy = (char*)malloc(sizeof(char) * len);
+	if (cpy == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		cpy[i] = str[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
+
+char	*ft_itoa_base(int value, int base)
+{
+	int		i;
+	int		n;
+	int		len;
+	char	*radix;
+	char	*result;
+
+	i = 0;
+	s = 0;
+	len = 0;
+	radix = "0123456789ABCDEF";
+	if (value == 0)
+		return (ft_strdup("0"));
+	if (base == 10 && value == -2147483648)
+		return (ft_strdup("-2147483648"));
+	len = ft_nbr_len(value, base);
+	if (base == 10 && value < 0)
+		s = 1;
+	result = (char*)malloc(sizeof(char) * (len + s + 1));
+	if (result == NULL)
+		return (NULL);
+	if (s == 1)
+	{
+		result[0] = '-';
+		len++;
+	}
+	result[len] = '\0';
+	if (value < 0)
+		value *= -1;
+	while (len > s)
+	{
+		result[len - 1] = radix[value % base];
+		value /= base;
+		len--;
+	}
+	return (result);
+}
